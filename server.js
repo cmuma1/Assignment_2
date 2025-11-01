1e0e89ea533218504766e1d71617041e1d6fa2f7
cmuma1 <cmuma1@my.centennialcollege.ca>
2025-10-31 22:53:05 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import Contact from "./models/contact.js";
+import contactRoutes from "./routes/contactRoutes.js";
+import projectRoutes from "./routes/projectRoutes.js";
+import qualificationRoutes from "./routes/qualificationRoutes.js";
+import meRoutes from "./routes/meRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+
+
+
+
+
+
+
+
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/contacts", contactRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/qualifications", qualificationRoutes);
+app.use("/api", meRoutes);
+app.use("/api/auth", authRoutes);
+
+
+
+
+
+
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("✅ MongoDB connected successfully!"))
+  .catch(err => console.error("❌ MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+  res.send("Server is running successfully!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
